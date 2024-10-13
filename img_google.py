248d001d3f408d22bf0b922d6ae06af288247026
Daanish Mittal <daanishmittal24@gmail.com>
2024-10-13 19:57:20 +0530

    updated

--- img_google.py
+++ img_google.py
@@ -5,22 +5,15 @@ import zipfile
 import shutil
 
 def clear_folder(directory):
-    """Clear the specified directory."""
     if os.path.exists(directory):
-        shutil.rmtree(directory)  # Remove the directory and its contents
-    os.makedirs(directory)  # Recreate the directory
+        shutil.rmtree(directory)
+    os.makedirs(directory)
 
 def download_images(query, limit):
-    # Directory to store downloaded images
     output_dir = 'downloaded_images'
-    clear_folder(output_dir)  # Clear the folder before downloading new images
-
-    # Initialize the Google Image Crawler
+    clear_folder(output_dir)
     crawler = GoogleImageCrawler(storage={'root_dir': output_dir})
-    # Start crawling and downloading images
     crawler.crawl(keyword=query, max_num=limit)
-
-    # Return the path to the downloaded images
     return output_dir
 
 def zip_images(directory):
@@ -33,7 +26,6 @@ def zip_images(directory):
                                                     os.path.join(directory, '..')))
     return zip_file_path
 
-# Streamlit user interface
 st.title("Google Images Downloader")
 st.write("Enter a search query to download images from Google Images.")
 
@@ -42,25 +34,27 @@ limit = st.number_input("Number of Images to Download", min_value=1, value=7)
 
 if st.button("Download Images"):
     if query:
-        try:
-            downloaded_images_path = download_images(query, limit)
-            st.success(f"Downloaded {limit} images for '{query}'!")
-
-            # Provide a button to download the zip file
-            zip_file_path = zip_images(downloaded_images_path)
-            with open(zip_file_path, 'rb') as f:
-                st.download_button(
-                    label="Download Images as ZIP",
-                    data=f,
-                    file_name='downloaded_images.zip',
-                    mime='application/zip'
-                )
-            
-            # Display downloaded images
-            for img in os.listdir(downloaded_images_path):
-                st.image(os.path.join(downloaded_images_path, img))
-
-        except Exception as e:
-            st.error(f"Error downloading images: {str(e)}")
+        with st.spinner("Downloading images..."):
+            try:
+                downloaded_images_path = download_images(query, limit)
+                st.success(f"Downloaded {limit} images for '{query}'!")
+                zip_file_path = zip_images(downloaded_images_path)
+                with open(zip_file_path, 'rb') as f:
+                    st.download_button(
+                        label="Download Images as ZIP",
+                        data=f,
+                        file_name='downloaded_images.zip',
+                        mime='application/zip'
+                    )
+                for img in os.listdir(downloaded_images_path):
+                    st.image(os.path.join(downloaded_images_path, img))
+            except Exception as e:
+                st.error(f"Error downloading images: {str(e)}")
     else:
         st.warning("Please enter a search query.")
+
+
+st.markdown("""
+    <br><br>
+    <h5 style='text-align: center;'>Made with ❤️ by <a href='https://github.com/daanishmittal24' target='_blank'> Daanish Mittal<br></a></h5>
+""", unsafe_allow_html=True)
